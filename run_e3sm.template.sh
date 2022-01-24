a962170264ef48af34c3688bc551535ab58506a4
Chongxing Fan <cxfan@umich.edu>
2022-01-24 13:47:02 -0800

    QRL test

--- run_e3sm.template.sh
+++ run_e3sm.template.sh
@@ -24,7 +24,7 @@ readonly PROJECT="m2136"
 # Simulation
 readonly COMPSET="WCYCL1850"
 readonly RESOLUTION="ne30pg2_EC30to60E2r2"
-readonly CASE_NAME="v2.LR.piControl.branch.test"
+readonly CASE_NAME="v2.LR.piControl.QRL.test"
 readonly CASE_GROUP="v2.LR"
 
 # Code and compilation
@@ -55,7 +55,7 @@ readonly CASE_ARCHIVE_DIR=${CASE_ROOT}/archive
 #  short tests: 'XS_2x5_ndays', 'XS_1x10_ndays', 'S_1x10_ndays',
 #               'M_1x10_ndays', 'M2_1x10_ndays', 'M80_1x10_ndays', 'L_1x10_ndays'
 #  or 'production' for full simulation
-readonly run='XS_2x5_ndays'
+readonly run='production'
 if [ "${run}" != "production" ]; then
 
   # Short test simulations
@@ -81,12 +81,12 @@ else
   # Production simulation
   readonly CASE_SCRIPTS_DIR=${CASE_ROOT}/case_scripts
   readonly CASE_RUN_DIR=${CASE_ROOT}/run
-  readonly PELAYOUT="L"
+  readonly PELAYOUT="M"
   readonly WALLTIME="0:30:00"
   readonly STOP_OPTION="ndays"
-  readonly STOP_N="5"
+  readonly STOP_N="1"
   readonly REST_OPTION="ndays"
-  readonly REST_N="5"
+  readonly REST_N="1"
   readonly RESUBMIT="0"
   readonly DO_SHORT_TERM_ARCHIVING=false
 fi
@@ -143,17 +143,11 @@ echo $'\n----- All done -----\n'
 user_nl() {
 
 cat << EOF >> user_nl_eam
- nhtfrq =   0,-24,-6,-6,-3,-24,0
- mfilt  = 1,30,120,120,240,30,1
- avgflag_pertape = 'A','A','I','A','A','A','I'
+ nhtfrq =   1
+ mfilt  = 48
+ avgflag_pertape = 'A'
  fexcl1 = 'CFAD_SR532_CAL', 'LINOZ_DO3', 'LINOZ_DO3_PSC', 'LINOZ_O3CLIM', 'LINOZ_O3COL', 'LINOZ_SSO3', 'hstobie_linoz'
- fincl1 = 'extinct_sw_inp','extinct_lw_bnd7','extinct_lw_inp','CLD_CAL', 'TREFMNAV', 'TREFMXAV'
- fincl2 = 'FLUT','PRECT','U200','V200','U850','V850','Z500','OMEGA500','UBOT','VBOT','TREFHT','TREFHTMN:M','TREFHTMX:X','QREFHT','TS','PS','TMQ','TUQ','TVQ','TOZ', 'FLDS', 'FLNS', 'FSDS', 'FSNS', 'SHFLX', 'LHFLX', 'TGCLDCWP', 'TGCLDIWP', 'TGCLDLWP', 'CLDTOT', 'T250', 'T200', 'T150', 'T100', 'T050', 'T025', 'T010', 'T005', 'T002', 'T001', 'TTOP', 'U250', 'U150', 'U100', 'U050', 'U025', 'U010', 'U005', 'U002', 'U001', 'UTOP', 'FSNT', 'FLNT'
- fincl3 = 'PSL','T200','T500','U850','V850','UBOT','VBOT','TREFHT', 'Z700', 'TBOT:M'
- fincl4 = 'FLUT','U200','U850','PRECT','OMEGA500'
- fincl5 = 'PRECT','PRECC','TUQ','TVQ','QFLX','SHFLX','U90M','V90M'
- fincl6 = 'CLDTOT_ISCCP','MEANCLDALB_ISCCP','MEANTAU_ISCCP','MEANPTOP_ISCCP','MEANTB_ISCCP','CLDTOT_CAL','CLDTOT_CAL_LIQ','CLDTOT_CAL_ICE','CLDTOT_CAL_UN','CLDHGH_CAL','CLDHGH_CAL_LIQ','CLDHGH_CAL_ICE','CLDHGH_CAL_UN','CLDMED_CAL','CLDMED_CAL_LIQ','CLDMED_CAL_ICE','CLDMED_CAL_UN','CLDLOW_CAL','CLDLOW_CAL_LIQ','CLDLOW_CAL_ICE','CLDLOW_CAL_UN'
- fincl7 = 'O3', 'PS', 'TROP_P'
+ fincl1 = 'QRLC','QRL','IWC','LWC','CLDICE','CLDLIQ'
 
 ! Additional retuning
  clubb_tk1 = 268.15D0
@@ -162,17 +156,10 @@ EOF
 
 cat << EOF >> user_nl_elm
  hist_dov2xy = .true.,.true.
- hist_fincl2 = 'H2OSNO', 'FSNO', 'QRUNOFF', 'QSNOMELT', 'FSNO_EFF', 'SNORDSL', 'SNOW', 'FSDS', 'FSR', 'FLDS', 'FIRE', 'FIRA'
- hist_mfilt = 1,365
- hist_nhtfrq = 0,-24
- hist_avgflag_pertape = 'A','A'
 EOF
 
 cat << EOF >> user_nl_mosart
- rtmhist_fincl2 = 'RIVER_DISCHARGE_OVER_LAND_LIQ'
- rtmhist_mfilt = 1,365
- rtmhist_ndens = 2
- rtmhist_nhtfrq = 0,-24
+
 EOF
 
 }
@@ -379,6 +366,9 @@ runtime_options() {
 
     # Coupler budgets (always on)
     ./xmlchange BUDGETS=TRUE
+
+    # Job queue (debug)
+    ./xmlchange JOB_QUEUE=debug
 
     # Set resubmissions
     if (( RESUBMIT > 0 )); then
