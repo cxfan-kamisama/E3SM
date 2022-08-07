54010a38ceb6dbe631f19893c4467a00fdcd9091
Chongxing Fan <cxfan@umich.edu>
2022-08-07 13:53:45 -0700

    Turn on all UMRad flags

--- run_e3sm.template.sh
+++ run_e3sm.template.sh
@@ -24,7 +24,7 @@ readonly PROJECT="m2136"
 # Simulation
 readonly COMPSET="WCYCL1850"
 readonly RESOLUTION="ne30pg2_EC30to60E2r2"
-readonly CASE_NAME="E3SMv2.pm.UMRad.piControl.0101.noScatEmis"
+readonly CASE_NAME="E3SMv2.pm.UMRad.piControl.0101.Full"
 readonly CASE_GROUP="E3SMv2.pm.UMRad"
 
 # Code and compilation
@@ -162,10 +162,10 @@ cat << EOF >> user_nl_eam
  fincl1 = 'QRLC','QRL','IWC','LWC','CLDICE','CLDLIQ'
 
 ! UMRad flags
- flag_mc6 = .false.
- flag_rtr2 = .false.
- flag_scat = .false.
- flag_emis = .false.
+ flag_mc6 = .true.
+ flag_rtr2 = .true.
+ flag_scat = .true.
+ flag_emis = .true.
  surf_emis_file = '$SURF_EMIS_FILE'
 
 ! Additional retuning
